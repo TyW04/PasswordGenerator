4361c2e74fbbaeee834b3de29ac8bc129f035074
TyW04 <tywagner04@gmail.com>
2025-10-12 14:33:03 -0600

    get_char_list function
    partially implemented and tested get_char_list() function.

--- passwordgen.cpp
+++ passwordgen.cpp
@@ -4,12 +4,30 @@
 
 using namespace std;
 
+vector<char> get_char_list(bool include_symbols, bool include_numbers, bool uppercase_only, bool lowercase_only) {
+    vector<char> char_list = {};
+
+    // Add characters a-z and A-Z
+    for (char c = 'a'; c <= 'z'; c++) {
+        char_list.push_back(c);
+    }
+
+    
+    for (char c = 'A'; c <= 'Z'; c++) {
+        char_list.push_back(c);
+    }
+    
+    return char_list;
+}
+
 int main() {
     random_device rd;
     mt19937 gen(rd());
 
-    for (int i = 0; i < 5; i++) {
-        cout << gen() << " ";
+    vector<char> char_list = get_char_list(true, true);
+
+    for (int n : char_list) {
+        cout << (char)n << ' ';
     }
     cout << endl;
     return 0;

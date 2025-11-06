7d1e9376a19d55cb2cad44a827c0da912fd7fc6d
TyW04 <tywagner04@gmail.com>
2025-11-05 23:46:30 -0700

    UI and app functionality changes

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-const { app, BrowserWindow } = require('electron')
-const { createReadStream } = require('original-fs')
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 600,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
-
   win.loadFile('index.html')
+  win.setMenu(null) // Remove menu bar
+  win.resizable = false;
 }
 
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', ()=> {
-    if (BrowserWindo.getAllWindows().length === 0) {
+    if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
   })
 })
 
+// User not on macOS, iOS, etc... and is closing the app
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()

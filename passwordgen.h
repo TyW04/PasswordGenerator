20d4fa0b25e46bf7fb42b0f9456fb293d30603fa
TyW04 <tywagner04@gmail.com>
2025-11-13 01:03:19 -0700

    Moved this

--- passwordgen.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include <string>
-
-std::string generate_password(int len, bool uppercase_only, bool lowercase_only, bool include_symbols, bool include_numbers);
-std::vector<char> get_char_list(bool include_symbols, bool include_numbers);
-std::string get_password(std::vector<char> char_list, int len, bool uppercase_only, bool lowercase_only);

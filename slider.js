c24c803ac93a84da4830cd0972e790baaabfc51a
TyW04 <tywagner04@gmail.com>
2025-11-13 01:03:58 -0700

    Moved

--- slider.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("password-length-slider");
-var output = document.getElementById("password-length");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}

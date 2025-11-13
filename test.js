df3bd63b8833eaa87e4d82e990febc1d8acd9683
TyW04 <tywagner04@gmail.com>
2025-11-13 01:03:45 -0700

    Test code for C++ integration

---
+++ test.js
@@ -0,0 +1,8 @@
+// Load our addon
+const myAddon = require('.')
+
+// Try the helloWorld function
+const result = myAddon.helloWorld('This is a test')
+
+// Should print: "Hello from C++! You said: This is a test"
+console.log(result)
